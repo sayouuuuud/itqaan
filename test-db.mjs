b07821df98cedc6bfffc3a656a1fbaa747e35f24
sayouuud <sayedelshazly2006@gmail.com>
2026-03-01 09:19:16 +0200

    one last

--- test-db.mjs
+++
@@ -1,8 +0,0 @@
-import { query } from './lib/db.js';
-
-async function checkUser() {
-    const users = await query('SELECT email FROM users WHERE email = $1', ['sayedelshazly2006@gmail.com']);
-    console.log('User found:', users);
-}
-
-checkUser();
--- test-templates.mjs
+++
@@ -1,8 +0,0 @@
-import { query } from './lib/db.js';
-
-async function checkTemplates() {
-    const templates = await query('SELECT template_key, is_active FROM email_templates');
-    console.log('Templates in DB:', templates);
-}
-
-checkTemplates();
